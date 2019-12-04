535e3d4d63bdae49c0e65fd9c597825b057ec428
Sonia Wittek <sonia@wittek.pl>
2019-12-04 13:59:28 +0100

    Z zajęć + UML

--- evolutionary_algorithm.py
+++ evolutionary_algorithm.py
@@ -1,20 +1,47 @@
-import classes
+from classes import *
+import abc
 
-def initiate(population_size: int)-> Population:
-    pass
+class Evolutionary_algorithm(abc):
+    def initiate(self, population_size: int) -> Population:
+        pass
 
-def modify()-> list:
-    pass
+    def mutate(self):
+        pass
 
-def stop(P,t):
+    def cross(self):
+        pass
 
-def evolutionary_algorithm():
-    t: int = 0
-    P = []
-    P[t] = initiate(population_size)
-    P[t].rate
-    while(not stop(P,t)):
-        t=t+1
-        modify(P[t])
-        P[t].rate
-    return(P[t])
+    def modify(self, parents: list) -> list:
+        for parent in parents:
+            pass
+        #!!!!!! Jak wybrać rodziców? Jak dobrać rodziców w pary? Najpierw mutacja czy najpierw krzyzowanie?
+        # Każdy z każdym, albo losowy z losowym
+
+    def stop(self, P, t):
+        pass
+
+    def selectParents(self, old_population: list) -> list:
+        pass
+
+    def rate(self):
+        pass
+
+    def selectSpecimen(self, population: list): #ja bym to wypieprzyla
+        rating = 0
+        bestSpecimen = population[0]
+        for specimen in population:
+            if self.rate(specimen)>rating:
+                self.rate = rating(specimen)
+                bestSpecimen = specimen
+        return bestSpecimen
+
+    def evolutionary_algorithm(self, population_size):
+        t: int = 0 # zmienić na zliczanie wywołania f oceny
+        P = self.initiate(population_size)
+        while (not self.stop(P, t)): # wartosc 20 jest jeszcze do ustalenia -> wartosc docelowa funkcji celu
+            t = t + 1
+            P = self.modify(self.selectParents(P))
+            # czy musi być ocena tutaj?
+            #ocena tylko tam gdzie trzeba
+        return (P)
+
