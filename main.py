9f9bdcbb3ee77f4ee6daf56cd80c2d833a4c5277
Jacek Lichwa <ytos.ventus@gmail.com>
2020-01-07 20:26:24 +0100

    Plotter cd.

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
 import evolutionary_algorithm
 from map import Map
 import configuration
-from output_presentation import *
+from output_presentation import Plotter
 
 
 def main():
     configuration.load_config_file()
     alg_map = Map()
-    data = evolutionary_algorithm.run_multiple_times(alg_map, 10)
+    data = evolutionary_algorithm.run_multiple_times(alg_map, 1000)
     print("DONE")
     [best_ratings, routes, time] = data
     for element in range(len(best_ratings)):
         print("Try", time[element], ":", best_ratings[element], routes[element])
 
-    fig, ax = plt.subplots()
-    ax.plot(time, best_ratings)
-
-    ax.set(xlabel='try', ylabel='best rating',
-           title='Super Sonia')
-    plt.show()
+    Plotter().style_setup()
+    Plotter().histogram(time, best_ratings)
 
 
 if __name__ == '__main__':
--- output_presentation.py
+++ output_presentation.py
@@ -2,11 +2,35 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import abc
 
 
-class Plotter:
+class Plotter(abc.ABC):
 
     def style_setup(self) -> None:
         sns.set(style="darkgrid")
         pass
+
+    def scatter_plot(self, x_data, y_data, x_axis_name="OX", y_axis_name="OY", plot_title="Scatter Plot") -> None:
+        fig, ax = plt.subplots()
+        ax.plot(x_data, y_data)
+        ax.set(xlabel=x_axis_name, ylabel=y_axis_name, title=plot_title)
+        plt.show()
+        return None
+
+    def line_plot(self, x_data, y_data, x_axis_name="OX", y_axis_name="OY", plot_title="Line Plot") -> None:
+        fig, ax = plt.subplots()
+        ax.plot(x_data, y_data)
+        ax.set(xlabel=x_axis_name, ylabel=y_axis_name, title=plot_title)
+        plt.show()
+        return None
+
+    def histogram(self, x_data, y_data, x_axis_name="OX", y_axis_name="OY", plot_title="Histogram Plot") -> None:
+        x_data.sort()
+        y_data.sort()
+        fig, ax = plt.subplots()
+        ax.hist(x_data)
+        ax.set(xlabel=x_axis_name, ylabel=y_axis_name, title=plot_title)
+        plt.show()
+        return None
 
