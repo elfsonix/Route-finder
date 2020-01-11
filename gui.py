f2d77f3de4dbbd074cfd559f938806f9f41827cf
Jacek Lichwa <ytos.ventus@gmail.com>
2020-01-12 00:20:41 +0100

    gui vol. 1

---
+++ gui.py
@@ -0,0 +1,16 @@
+from evolutionary_algorithm import EAlgorithm
+from multi_track_drifting import MultiInstanceEAlgorithm
+from output_presentation import Plotter
+
+class GUI:
+    def __init__(self):
+        pass
+
+    def main_menu(self):
+        pass
+
+    def multi_instance_menu(self):
+        pass
+
+    def custom_instance_menu(self):
+        pass
